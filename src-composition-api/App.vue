dd68084628c52c49470505e44b8b5b538d6febbe
kristjan kangro <kristjan.kangro@visma.com>
2025-07-14 01:55:07 +0300

    mod38

--- src-composition-api/App.vue
+++ src-composition-api/App.vue
@@ -1,15 +1,27 @@
 <script lang="ts" setup>
-import {ref} from "vue";
-
+import {computed, reactive, ref} from "vue";
+// reactive is used for objects, ref is used for primitive values
 const count = ref<number>(0);
+const numbers = reactive({a: 0, b: 0})
 
 const increment = () => {
   count.value++;
 };
+const increase = (key: 'a' | 'b') => {
+  numbers[key]++;
+};
+
+// Computed property to calculate the total
+const total = computed(() => {
+  return count.value + numbers.a + numbers.b;
+});
 </script>
 
 <template>
   <button @click="increment">{{ count }}</button>
+  <button @click="increase('a')">{{ numbers.a }}</button>
+  <button @click="increase('b')">{{ numbers.b }}</button>
+  <p>{{ total }}</p>
 </template>
 
 <style scoped>
