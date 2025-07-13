ea8c888fb53803a17c8d8a9046d87c786bac1177
kristjan kangro <kristjan.kangro@visma.com>
2025-07-13 11:47:49 +0300

    mo19

--- src-form-validation/App.vue
+++ src-form-validation/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import btn from './components/Btn.vue'
+import inp from './components/Input.vue'
 </script>
 
 <template>
 	<p>Form validation</p>
+  <inp name="Username"/>
 	<btn bg="blue" color="white" :disabled="false" text="Click Me" />
 </template>
 
---
+++ src-form-validation/components/Input.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ name: string }>()
+</script>
+
+<template>
+  <div class="label">
+    <label :for="name">{{ name }}</label>
+    <input :id="name" :name="name"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
